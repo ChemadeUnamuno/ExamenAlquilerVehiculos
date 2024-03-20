0c28df0ac5bcff00163ee6b7b6684bfdb15188f3
unknown <jcarmor0807@g.educaand.es>
2024-03-20 10:04:50 +0100

    Clase Camion corregida

--- src/modelo/Camion.java
+++ src/modelo/Camion.java
@@ -1,4 +1,25 @@
-
-public class Camion {
+package modelo;
+public class Camion extends Vehiculo{
+private int pesoMaximo;
+private float longitudMaxima;
+	 
+	public Camion(String matricula, String modelo, String marca, float kmRecorridos, float precioDia, String tipoMotor,
+			int pesoMaximo, float longitudMaxima) {
+		super(matricula, modelo, marca, kmRecorridos, precioDia, tipoMotor);
+		this.pesoMaximo = pesoMaximo;
+		this.longitudMaxima = longitudMaxima;
+	}
+	public int getPesoMaximo() {
+		return pesoMaximo;
+	}
+	public void setPesoMaximo(int pesoMaximo) {
+		this.pesoMaximo = pesoMaximo;
+	}
+	public float getLongitudMaxima() {
+		return longitudMaxima;
+	}
+	public void setLongitudMaxima(float longitudMaxima) {
+		this.longitudMaxima = longitudMaxima;
+	}
 
 }
